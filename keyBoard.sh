fad72ff58be44cd8f3c6d62909432a44702d2f24
clwater <gzb@clwater.top>
2023-02-28 10:06:52 +0800

    fix: choose

--- keyBoard.sh
+++ keyBoard.sh
@@ -7,7 +7,7 @@ KeyBoard_RIGHT="KeyBoard_RIGHT"
 KeyBoard_SPACE="KeyBoard_SPACE"
 KeyBoard_ENTER="KeyBoard_ENTER"
 
-function Input.input() {
+function Input.input(){
 
     unset K1 K2 K3
     read -s -N1 -p ""
@@ -30,48 +30,48 @@ function Input.input() {
     fi
 
     case "$key" in
-    $'\x1b\x5b\x32\x7e') # 插入
-        echo Insert Key
-        ;;
-    $'\x1b\x5b\x33\x7e') # 删除
-        echo Delete Key
-        ;;
-    $'\x1b\x5b\x31\x7e') # 数字7上的Home键
-        echo Home Key
-        ;;
-    $'\x1b\x5b\x34\x7e') # 数字1上的End键
-        echo End Key
-        ;;
-    $'\x1b\x5b\x35\x7e') # 上翻页
-        echo Page_Up
-        ;;
-    $'\x1b\x5b\x36\x7e') # 下翻页
-        echo Page_Down
-        ;;
-    $'\x1b\x5b\x41') # 上箭头
-        echo $KeyBoard_UP
-        ;;
-    $'\x1b\x5b\x42') # 下箭头
-        echo $KeyBoard_DOWN
-        ;;
-    $'\x1b\x5b\x43') # 右箭头
-        echo $KeyBoard_RIGHT
-        ;;
-    $'\x1b\x5b\x44') # 左箭头
-        echo $KeyBoard_LEFT
-        ;;
-    $'\x09') # 制表符
-        echo Tab Key
-        ;;
-    $'\x0a') # 回车
-        echo $KeyBoard_ENTER
-        ;;
-    $'\x1b') # ESC
-        echo Escape Key
-        ;;
-    $'\x20') # 空格
-        echo $KeyBoard_SPACE
-        ;;
+        $'\x1b\x5b\x32\x7e') # 插入
+            echo Insert Key
+            ;;
+        $'\x1b\x5b\x33\x7e') # 删除
+            echo Delete Key
+            ;;
+        $'\x1b\x5b\x31\x7e') # 数字7上的Home键
+            echo Home Key
+            ;;
+        $'\x1b\x5b\x34\x7e') # 数字1上的End键
+            echo End Key
+            ;;
+        $'\x1b\x5b\x35\x7e') # 上翻页
+            echo Page_Up
+            ;;
+        $'\x1b\x5b\x36\x7e') # 下翻页
+            echo Page_Down
+            ;;
+        $'\x1b\x5b\x41') # 上箭头
+            echo $KeyBoard_UP
+            ;;
+        $'\x1b\x5b\x42') # 下箭头
+            echo $KeyBoard_DOWN
+            ;;
+        $'\x1b\x5b\x43') # 右箭头
+            echo $KeyBoard_RIGHT
+            ;;
+        $'\x1b\x5b\x44') # 左箭头
+            echo $KeyBoard_LEFT
+            ;;
+        $'\x09') # 制表符
+            echo Tab Key
+            ;;
+        $'\x0a') # 回车
+            echo $KeyBoard_ENTER
+            ;;
+        $'\x1b') # ESC
+            echo Escape Key
+            ;;
+        $'\x20') # 空格
+            echo $KeyBoard_SPACE
+            ;;
     esac
 
 }
