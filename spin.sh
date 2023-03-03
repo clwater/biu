8483e1a04c4385313934fbae057675b560a754d3
全村人的希望 <gzb@clwater.top>
2023-03-03 21:59:28 +0800

    wip: spin

--- spin.sh
+++ spin.sh
@@ -2,24 +2,23 @@
 
 # use example from https://github.com/charmbracelet/bubbles/blob/master/spinner/spinner.go
 
-function showMoon(){
-    local spin=(🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘)
-    local spinNum=${#spin[@]}
-    local sleepTime=$(awk "BEGIN{print 1/$spinNum}")
-    local index=0
-    while true; do
-        echo -n ${spin[$index]}
-        index=$((index+1))
-        echo -e "\r\c"
-        if [ $index -eq $spinNum ]; then
-            index=0
-        fi
-        sleep $sleepTime
-    done
-}
 
-function ShowDot(){
-    local spin=(⣾ ⣽  ⣻  ⢿  ⡿  ⣟  ⣯  ⣷)
+declare -A Spins
+
+Spins["moon"]="🌑 🌒 🌓 🌔 🌕 🌖 🌗 🌘"
+Spins["dot"]="⣾ ⣽  ⣻  ⢿  ⡿  ⣟  ⣯  ⣷"
+Spins["line"]="| / - \\"
+Spins["mini"]="⠋ ⠙ ⠹ ⠸ ⠼ ⠴ ⠦ ⠧ ⠇ ⠏"
+Spins["jump"]="⢄ ⢂ ⢁ ⡁ ⡈ ⡐ ⡠"
+Spins["pluse"]="█ ▓ ▒ ░"
+Spins["points"]="∙∙∙ ●∙∙ ∙●∙ ∙∙●"
+Spins["glove"]="🌍 🌎 🌏"
+Spins["monkey"]="🙈 🙉 🙊"
+Spins["meter"]="▱▱▱ ▰▱▱ ▰▰▱ ▰▰▰ ▰▰▱ ▰▱▱ ▱▱▱"
+Spins["hamburger"]="☱ ☲ ☴ ☲"
+
+function showSpin(){
+    local spin=(`echo ${Spins[$1]}`)  
     local spinNum=${#spin[@]}
     local sleepTime=$(awk "BEGIN{print 1/$spinNum}")
     local index=0
@@ -37,7 +36,8 @@ function ShowDot(){
 # run choose
 function Spin.run() {
     echo "Spin run"
-    showMoon
+    Utils.hideCursor
+    showSpin "moon"
 }
 
 function Spin.help(){
